2cb94a85849cc9457ee4ccf01fa95bc275cb7560
Naturean <112891509+Naturean@users.noreply.github.com>
2026-04-07 16:53:02 +0800

    feat(algorithm): add Map version of lengthOfLongestSubstring function

--- algorithm/3_lengthOfLongestSubstring.js
+++ algorithm/3_lengthOfLongestSubstring.js
@@ -34,3 +34,24 @@ var lengthOfLongestSubstring = function (s) {
 
   return max;
 };
+
+// Map版本
+var lengthOfLongestSubstring = function (s) {
+  // Map Version
+  let left = 0,
+    max = 0;
+  // 跳跃表，记录字母对应的下标，后续发现重复则据此收缩窗口左边界
+  const map = new Map();
+  for (let right = 0; right < s.length; right++) {
+    const char = s[right];
+    if (map.has(char)) {
+      left = Math.max(left, map.get(char) + 1);
+    }
+    max = Math.max(max, right - left + 1);
+    // 更新跳跃表
+    // 为何省去了删除重复字符的步骤？因为不再需要Set的方法来判断当前子串是否存在重复字符
+    // 每次跳过上一个字符，并更新跳跃表后，总是保证该字符在当前子串中只出现一次
+    map.set(char, right);
+  }
+  return max;
+};
